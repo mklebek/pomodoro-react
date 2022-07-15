01f329de602d06d028c1c5d0ee9d7f5d8a068271
Michal Klebek <michal.klebek@gmail.com>
2022-07-15 11:34:33 +0200

    Handle expired countdown

--- src/app/shared/timer/Timer.js
+++ src/app/shared/timer/Timer.js
@@ -5,9 +5,15 @@ const Timer = () => {
   const [minutes, seconds] = useCountdown(countDownDate);
 
   return (
-    <h1>
-      {minutes}:{seconds}
-    </h1>
+    <div>
+      {minutes + seconds <= 0 ? (
+        <h1>Expired!</h1>
+      ) : (
+        <h1>
+          {minutes}:{seconds}
+        </h1>
+      )}
+    </div>
   );
 };
 
